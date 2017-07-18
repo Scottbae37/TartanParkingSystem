39884bcb2078252b7e4161f70e79298d0279f799
kyungman.yu <yu820911@gmail.com>
2017-07-19 06:44:48 +0900

    [kyungman]create Adminservice

---
+++ src/edu/cmu/tartan/service/AdminService.java
@@ -0,0 +1,30 @@
+package edu.cmu.tartan.service;
+
+import java.util.HashMap;
+
+/**
+ * Created by kyungman.yu on 2017-07-19.
+ */
+public class AdminService extends TartanService {
+    @Override
+    public void handleMessage(HashMap<String, Object> message) {
+
+    }
+
+    @Override
+    public void terminate() {
+
+    }
+
+    @Override
+    public void run() {
+
+
+    }
+
+    private void handleCreateNewReservation(HashMap<String, Object> request) {
+
+
+
+    }
+}
