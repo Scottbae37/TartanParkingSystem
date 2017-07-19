8afecea556d0cee7d4b5bf5b61e14f3fd315cd18
wonil.hwang <wonil.hwang@lge.com>
2017-07-19 15:30:05 -0400

    [Admin] implemented authentication using hash and test

--- src/edu/cmu/tartan/service/AdminService.java
+++ src/edu/cmu/tartan/service/AdminService.java
@@ -1,5 +1,9 @@
 package edu.cmu.tartan.service;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 import java.util.HashMap;
 
 /**
@@ -26,5 +30,28 @@ public class AdminService extends TartanService {
 
 
 
+    }
+
+    public boolean authenticate(String id, String pwd) {
+        String inputPwdEncoded = hashPassword(pwd);
+        String adminAuth[] = getAdminAuth();
+        return id.equals(adminAuth[0]) && inputPwdEncoded.equals(adminAuth[1]);
+    }
+
+    public String[] getAdminAuth() {
+        String adminAuth[] = { "admin", "BZoAGSWS1URLwMqtcgP5i1BjMuLPers11oTqm/fBjwg=" };
+        return adminAuth;
+    }
+
+    public String hashPassword(String pwd) {
+        String encoded = null;
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(pwd.getBytes(StandardCharsets.UTF_8));
+            encoded = Base64.getEncoder().encodeToString(hash);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return encoded;
     }
 }
