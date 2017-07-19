88d0d7f1ee4d722110d431df484172bf56f81678
JaeseungBae <jaeseung.bae@lge.com>
2017-07-19 15:11:40 -0400

    [Test][ParkingService] Mockito example for supress method(in Constructor) and mock unused method for this test.

--- test/edu/cmu/tartan/service/ParkingServiceTest.java
+++ test/edu/cmu/tartan/service/ParkingServiceTest.java
@@ -1,43 +1,89 @@
 package edu.cmu.tartan.service;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.util.HashMap;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import static org.mockito.Matchers.any;
+import static org.powermock.api.support.membermodification.MemberMatcher.methods;
+import static org.powermock.api.support.membermodification.MemberModifier.suppress;
+
 /**
- * Created by chongjae.yoo on 2017-07-18.
+ * Created by jaeseung.bae on 7/18/2017.
  */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(ParkingService.class)
 public class ParkingServiceTest {
-    @org.junit.Before
+
+    @Before
     public void setUp() throws Exception {
     }
 
-    @org.junit.After
+    @After
     public void tearDown() throws Exception {
     }
 
-    @org.junit.Test
+    @Test
     public void update() throws Exception {
     }
 
-    @org.junit.Test
+    @Test
     public void connectToGarage() throws Exception {
     }
 
-    @org.junit.Test
+    @Test
     public void disconnectFromGarage() throws Exception {
     }
 
-    @org.junit.Test
+    @Test
     public void finalize() throws Exception {
     }
 
-    @org.junit.Test
+    @Test
     public void run() throws Exception {
     }
 
-    @org.junit.Test
+    @Test
     public void handleMessage() throws Exception {
     }
 
-    @org.junit.Test
+    @Test
     public void terminate() throws Exception {
     }
 
+    @Test
+    public void sendMessage() throws Exception {
+    }
+
+    @Test
+    public void onMessage() throws Exception {
+    }
+
+    @Test
+    public void init() throws Exception {
+        /* SetUP */
+        /* Put two annotations below, See above
+        * @RunWith(PowerMockRunner.class)
+          @PrepareForTest(ParkingService.class)
+        * */
+        suppress(methods(ParkingService.class, "init"));
+        ParkingService mockPar = new ParkingService();
+        ParkingService spy = Mockito.spy(mockPar);
+        Mockito.doNothing().when(spy).sendMessage((String)any(), (HashMap)any());
+        /* Exercise */
+        spy.update(null, "Nothing");
+        spy.sendMessage("Aaa", null);
+
+        /* Verify */
+//        fail();
+        /* Tear-down */
+    }
 }
