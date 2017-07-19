2464645fe593d39818c48bf2ae74fd8c55051fc5
chongjae.yoo <chongjae.yoo@lge.com>
2017-07-20 03:36:53 +0900

    [JUnit] Setup TartanServiceMessageBus
    [Desc]
    
    Set TartanServiceMessageBus to mock for independency test

--- test/edu/cmu/tartan/service/KioskServiceTest.java
+++ test/edu/cmu/tartan/service/KioskServiceTest.java
@@ -1,16 +1,39 @@
 package edu.cmu.tartan.service;
-import edu.cmu.tartan.TartanKioskWindow;
 
+import edu.cmu.tartan.edu.cmu.tartan.reservation.Reservation;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import javax.jms.MessageConsumer;
+import javax.jms.MessageProducer;
 import java.util.HashMap;
-
-import static org.mockito.Mockito.*;
+import java.util.Vector;
 
 /**
  * Created by chongjae.yoo on 2017-07-18.
  */
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(TartanServiceMessageBus.class)
 public class KioskServiceTest {
+
+    KioskService kioskService;
+    TartanServiceMessageBus msgBus;
+    MessageConsumer consumer;
+    MessageProducer producer;
+
     @org.junit.Before
     public void setUp() throws Exception {
+        msgBus = PowerMockito.mock(TartanServiceMessageBus.class);
+        consumer = PowerMockito.mock(MessageConsumer.class);
+        producer = PowerMockito.mock(MessageProducer.class);
+        PowerMockito.when(msgBus.getConsumer(TartanServiceMessageBus.TARTAN_TOPIC)).thenReturn(consumer);
+        PowerMockito.when(msgBus.getProducer(TartanServiceMessageBus.TARTAN_TOPIC)).thenReturn(producer);
+        PowerMockito.mockStatic(TartanServiceMessageBus.class);
+        PowerMockito.when(TartanServiceMessageBus.connect()).thenReturn(msgBus);
+        kioskService = new KioskService();
     }
 
     @org.junit.After
@@ -19,11 +42,10 @@ public class KioskServiceTest {
 
     @org.junit.Test
     public void handleMessage() throws Exception {
-        ParkingService parkingService = mock(ParkingService.class);
         HashMap<String, Object> msg = new HashMap<String, Object>();
         msg.put(TartanParams.COMMAND, TartanParams.MSG_REDEEM_RSVP);
-        parkingService.handleMessage(msg);
-        verify(parkingService).handleMessage(msg);
+        msg.put(TartanParams.PAYLOAD, new Vector<Reservation>());
+        kioskService.handleMessage(msg);
     }
 
     @org.junit.Test
