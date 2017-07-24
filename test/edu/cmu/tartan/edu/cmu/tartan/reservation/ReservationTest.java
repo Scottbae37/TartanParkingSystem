11664b18179bf94b59e48d0cc2c6ce5a2a686301
JaeseungBae <jaeseung.bae@lge.com>
2017-07-23 22:29:43 -0400

    [Upload] New code for Jenkins test.

---
+++ test/edu/cmu/tartan/edu/cmu/tartan/reservation/ReservationTest.java
@@ -0,0 +1,109 @@
+package edu.cmu.tartan.edu.cmu.tartan.reservation;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by jaeseung.bae on 7/23/2017.
+ */
+public class ReservationTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getSpotId() throws Exception {
+    }
+
+    @Test
+    public void isComplete() throws Exception {
+    }
+
+    @Test
+    public void setSpotId() throws Exception {
+    }
+
+    @Test
+    public void getStartTime() throws Exception {
+    }
+
+    @Test
+    public void setStartTime() throws Exception {
+    }
+
+    @Test
+    public void setStartTime1() throws Exception {
+    }
+
+    @Test
+    public void getEndTime() throws Exception {
+    }
+
+    @Test
+    public void setEndTime() throws Exception {
+    }
+
+    @Test
+    public void setEndTime1() throws Exception {
+    }
+
+    @Test
+    public void setIsRedeemed() throws Exception {
+    }
+
+    @Test
+    public void getIsRedeemed() throws Exception {
+    }
+
+    @Test
+    public void setPayment() throws Exception {
+    }
+
+    @Test
+    public void getPayment() throws Exception {
+    }
+
+    @Test
+    public void setIsPaid() throws Exception {
+    }
+
+    @Test
+    public void getIsPaid() throws Exception {
+    }
+
+    @Test
+    public void getCustomerName() throws Exception {
+    }
+
+    @Test
+    public void setCustomerName() throws Exception {
+    }
+
+    @Test
+    public void getVehicleID() throws Exception {
+    }
+
+    @Test
+    public void setVehicleID() throws Exception {
+    }
+
+    @Test
+    public void toString() throws Exception {
+    }
+
+    @Test
+    public void equals() throws Exception {
+    }
+
+    @Test
+    public void hashCode() throws Exception {
+    }
+
+}
