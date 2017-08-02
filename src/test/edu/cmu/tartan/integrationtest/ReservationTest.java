dd110a32801458ebc5b6bff69bf353d3814f4269
mailofnsh <mailofnsh@gmail.com>
2017-08-02 00:25:45 -0400

    Reservation Integration test2 for Test

--- src/test/edu/cmu/tartan/integrationtest/ReservationTest.java
+++ src/test/edu/cmu/tartan/integrationtest/ReservationTest.java
@@ -16,8 +16,12 @@ import org.powermock.reflect.Whitebox;
 
 import javax.jms.MessageConsumer;
 import javax.jms.MessageProducer;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.ZoneId;
 import java.util.*;
 
+@PrepareForTest({ReservationService.class, TartanServiceMessageBus.class, ReservationStore.class})
 @RunWith(PowerMockRunner.class)
 public class ReservationTest {
 
@@ -38,7 +42,7 @@ public class ReservationTest {
         PowerMockito.mockStatic(TartanServiceMessageBus.class);
         PowerMockito.when(TartanServiceMessageBus.connect()).thenReturn(msgBus);
 
-        reservationService = Mockito.spy(new ReservationService("./"));
+        reservationService = new ReservationService("./");
         Whitebox.setInternalState(reservationService, "rsvpStore", reservationStore);
     }
 
@@ -70,15 +74,14 @@ public class ReservationTest {
     /**
      * TS01-TC01 - Reservation try when all spots are already full.
      */
-    @PrepareForTest({Calendar.class, ReservationService.class, TartanServiceMessageBus.class, ReservationStore.class})
     @Test
     public void test_Reservation_try_when_all_spots_are_already_full() {
-        Calendar cal = Calendar.getInstance(); //  Current date is 07-27 15:00.
-        cal.set(2017, 5, 27, 15, 0);
-        Date cur = cal.getTime();
         Vector list = new Vector<Reservation>();
+
+        LocalDateTime ldt = LocalDateTime.of(2017, Month.JULY, 27, 15, 0);
+        Date d = Date.from(ldt.atZone(ZoneId.systemDefault()).toInstant());
         PowerMockito.mockStatic(Calendar.class);
-        PowerMockito.when(Calendar.getInstance()).thenReturn(cal);
+        Mockito.when(Calendar.getInstance().getTime()).thenReturn(d);
 
         // set available total garage
         reservationService.handleMessage(newMessage_MSG_GET_PARKING_SPOTS());
@@ -93,7 +96,6 @@ public class ReservationTest {
         Assert.assertEquals(Boolean.TRUE, aaa.isComplete());
         Mockito.verify(reservationStore, Mockito.times(1)).getReservations();
         list.add(aaa);
-        // --------------------
 
         Reservation bbb = new Reservation();
         bbb.setCustomerName("bbb");
@@ -105,8 +107,6 @@ public class ReservationTest {
         Assert.assertEquals(Boolean.TRUE, bbb.isComplete());
         Mockito.verify(reservationStore, Mockito.times(2)).getReservations();
         list.add(bbb);
-        // -------------------
-
 
         Reservation ccc = new Reservation();
         ccc.setCustomerName("ccc");
@@ -140,21 +140,20 @@ public class ReservationTest {
         reservationService.handleMessage(newMessage_MSG_NEW_RSVP(eee));
         Assert.assertEquals(Boolean.FALSE, eee.isComplete());
         Mockito.verify(reservationStore, Mockito.times(5)).getReservations();
-
+        Mockito.verify(msgBus).generateMessage(newMessage_Error_ReservationService_INVALID_SPOT(), null);
     }
 
     /**
      * TS01-TC02 - Reservation try with invalid name
      */
-    @PrepareForTest({Calendar.class, ReservationService.class, TartanServiceMessageBus.class, ReservationStore.class})
     @Test
     public void test_Reservation_try_with_invalid_name() {
-        Calendar cal = Calendar.getInstance(); //  Current date is 07-27 15:00.
-        cal.set(2017, 5, 27, 15, 0);
-        Date cur = cal.getTime();
         Vector list = new Vector<Reservation>();
+
+        LocalDateTime ldt = LocalDateTime.of(2017, Month.JULY, 27, 15, 0);
+        Date d = Date.from(ldt.atZone(ZoneId.systemDefault()).toInstant());
         PowerMockito.mockStatic(Calendar.class);
-        PowerMockito.when(Calendar.getInstance()).thenReturn(cal);
+        Mockito.when(Calendar.getInstance().getTime()).thenReturn(d);
 
         // set available total garage
         reservationService.handleMessage(newMessage_MSG_GET_PARKING_SPOTS());
@@ -167,15 +166,229 @@ public class ReservationTest {
         Mockito.when(reservationStore.getReservations()).thenReturn(list);
         reservationService.handleMessage(newMessage_MSG_NEW_RSVP(aaa));
         Assert.assertEquals(Boolean.FALSE, aaa.isComplete());
-        Mockito.verify(reservationService).sendMessage(null, newMessage_Error_ReservationService_verifyReservation());
+        Mockito.verify(msgBus).generateMessage(newMessage_Error_ReservationService_verifyReservation(), null);
     }
 
-    private HashMap<String, Object> newMessage_Error_ReservationService_verifyReservation(){
+    /**
+     * TS01-TC03 - Reservation try with invalid vehicle ID
+     */
+    @Test
+    public void test_Reservation_try_with_invalid_vehicle_ID() {
+        Vector list = new Vector<Reservation>();
+
+        LocalDateTime ldt = LocalDateTime.of(2017, Month.JULY, 27, 15, 0);
+        Date d = Date.from(ldt.atZone(ZoneId.systemDefault()).toInstant());
+        PowerMockito.mockStatic(Calendar.class);
+        Mockito.when(Calendar.getInstance().getTime()).thenReturn(d);
+
+        // set available total garage
+        reservationService.handleMessage(newMessage_MSG_GET_PARKING_SPOTS());
+
+        Reservation aaa = new Reservation();
+        aaa.setCustomerName("somebody");
+        aaa.setVehicleID(" ");
+        aaa.setStartTime("2017:07:27:17:00");//"yyyy:MM:dd:HH:mm"
+        aaa.setEndTime("2017:07:27:18:00");
+        Mockito.when(reservationStore.getReservations()).thenReturn(list);
+        reservationService.handleMessage(newMessage_MSG_NEW_RSVP(aaa));
+        Assert.assertEquals(Boolean.FALSE, aaa.isComplete());
+        Mockito.verify(msgBus).generateMessage(newMessage_Error_ReservationService_verifyReservation(), null);
+    }
+
+
+    /**
+     * TS01-TC04 - Reservation try when start time < current time
+     */
+    @Test
+    public void test_Reservation_try_when_start_time_is_less_than_current_time() {
+        Vector list = new Vector<Reservation>();
+
+        LocalDateTime ldt = LocalDateTime.of(2017, Month.JULY, 27, 15, 0);
+        Date d = Date.from(ldt.atZone(ZoneId.systemDefault()).toInstant());
+        PowerMockito.mockStatic(Calendar.class);
+        Mockito.when(Calendar.getInstance().getTime()).thenReturn(d);
+
+        // set available total garage
+        reservationService.handleMessage(newMessage_MSG_GET_PARKING_SPOTS());
+
+        Reservation aaa = new Reservation();
+        aaa.setCustomerName("somebody");
+        aaa.setVehicleID(" ");
+        aaa.setStartTime("2017:07:27:13:00");//"yyyy:MM:dd:HH:mm"
+        aaa.setEndTime("2017:07:27:18:00");
+        Mockito.when(reservationStore.getReservations()).thenReturn(list);
+        reservationService.handleMessage(newMessage_MSG_NEW_RSVP(aaa));
+        Assert.assertEquals(Boolean.FALSE, aaa.isComplete());
+        Mockito.verify(msgBus).generateMessage(newMessage_Error_ReservationService_verifyReservation(), null);
+    }
+
+    private HashMap<String, Object> newMessage_Error_ReservationService_verifyReservation() {
         HashMap<String, Object> response = new HashMap<String, Object>();
 
-        response.put(TartanParams.SOURCE_ID,"RsvpService");
+        response.put(TartanParams.SOURCE_ID, "RsvpService");
         response.put(TartanParams.COMMAND, TartanParams.ERROR);
         response.put(TartanParams.PAYLOAD, "Reservation is invalid, please submit a valid reservation");
         return response;
+    }
+
+    private HashMap<String, Object> newMessage_Error_ReservationService_INVALID_SPOT() {
+        HashMap<String, Object> response = new HashMap<String, Object>();
+
+        response.put(TartanParams.SOURCE_ID, "RsvpService");
+        response.put(TartanParams.COMMAND, TartanParams.ERROR);
+        response.put(TartanParams.PAYLOAD, "Parking space unavailable at desired time");
+        return response;
+    }
+
+
+    /**
+     * TS01-TC05 - Reservation try when start time >= current time + week
+     */
+    @Test
+    public void test_Reservation_try_when_start_time_or_more_current_time_week() {
+        Vector list = new Vector<Reservation>();
+
+        LocalDateTime ldt = LocalDateTime.of(2017, Month.JULY, 27, 15, 0);
+        Date d = Date.from(ldt.atZone(ZoneId.systemDefault()).toInstant());
+
+        PowerMockito.mockStatic(Calendar.class);
+        Mockito.when(Calendar.getInstance().getTime()).thenReturn(d);
+
+        PowerMockito.mockStatic(System.class);
+        Mockito.when(System.currentTimeMillis()).thenReturn(d.getTime());
+
+        // set available total garage
+        reservationService.handleMessage(newMessage_MSG_GET_PARKING_SPOTS());
+
+        Reservation aaa = new Reservation();
+        aaa.setCustomerName("somebody");
+        aaa.setVehicleID("5tgb");
+        aaa.setStartTime("2017:08:03:13:00");//"yyyy:MM:dd:HH:mm"
+        aaa.setEndTime("2017:08:03:18:00");
+        Mockito.when(reservationStore.getReservations()).thenReturn(list);
+        reservationService.handleMessage(newMessage_MSG_NEW_RSVP(aaa));
+        Assert.assertEquals(Boolean.FALSE, aaa.isComplete());
+        Mockito.verify(msgBus).generateMessage(newMessage_Error_ReservationService_verifyReservation(), null);
+    }
+
+    /**
+     * TS01-TC06 - Reservation try when start time >= current time + week
+     */
+    @Test
+    public void test_Reservation_try_when_start_time_or_more_current_time_week2() {
+        Vector list = new Vector<Reservation>();
+
+        LocalDateTime ldt = LocalDateTime.of(2017, Month.AUGUST, 1, 15, 0);
+        Date d = Date.from(ldt.atZone(ZoneId.systemDefault()).toInstant());
+
+        PowerMockito.mockStatic(Calendar.class);
+        Mockito.when(Calendar.getInstance().getTime()).thenReturn(d);
+
+        PowerMockito.mockStatic(System.class);
+        Mockito.when(System.currentTimeMillis()).thenReturn(d.getTime());
+
+        // set available total garage
+        reservationService.handleMessage(newMessage_MSG_GET_PARKING_SPOTS());
+
+        Reservation aaa = new Reservation();
+        aaa.setCustomerName("somebody");
+        aaa.setVehicleID("5tgb");
+        aaa.setStartTime("2017:08:08:15:00");//"yyyy:MM:dd:HH:mm"
+        aaa.setEndTime("2017:08:08:16:00");
+        Mockito.when(reservationStore.getReservations()).thenReturn(list);
+        reservationService.handleMessage(newMessage_MSG_NEW_RSVP(aaa));
+        Assert.assertEquals(Boolean.FALSE, aaa.isComplete());
+        Mockito.verify(msgBus).generateMessage(newMessage_Error_ReservationService_verifyReservation(), null);
+    }
+
+
+    /**
+     * TS01-TC07 - Reservation try when start time >= end time
+     */
+    @Test
+    public void test_Reservation_try_when_start_time_or_more_than_end_time() {
+        Vector list = new Vector<Reservation>();
+
+        LocalDateTime ldt = LocalDateTime.of(2017, Month.AUGUST, 1, 15, 0);
+        Date d = Date.from(ldt.atZone(ZoneId.systemDefault()).toInstant());
+
+        PowerMockito.mockStatic(Calendar.class);
+        Mockito.when(Calendar.getInstance().getTime()).thenReturn(d);
+
+        PowerMockito.mockStatic(System.class);
+        Mockito.when(System.currentTimeMillis()).thenReturn(d.getTime());
+
+        // set available total garage
+        reservationService.handleMessage(newMessage_MSG_GET_PARKING_SPOTS());
+
+        Reservation aaa = new Reservation();
+        aaa.setCustomerName("somebody");
+        aaa.setVehicleID("5tgb");
+        aaa.setStartTime("2017:08:01:15:00");//"yyyy:MM:dd:HH:mm"
+        aaa.setEndTime("2017:08:01:15:00");
+        Mockito.when(reservationStore.getReservations()).thenReturn(list);
+        reservationService.handleMessage(newMessage_MSG_NEW_RSVP(aaa));
+        Assert.assertEquals(Boolean.FALSE, aaa.isComplete());
+        Mockito.verify(msgBus).generateMessage(newMessage_Error_ReservationService_verifyReservation(), null);
+    }
+
+    /**
+     * TS01-TC08 - Reservation try when end time > current time + week
+     */
+    @Test
+    public void test_Reservation_try_when_end_time_or_more_than_cur_plus_week() {
+        Vector list = new Vector<Reservation>();
+
+        LocalDateTime ldt = LocalDateTime.of(2017, Month.AUGUST, 1, 15, 0);
+        Date d = Date.from(ldt.atZone(ZoneId.systemDefault()).toInstant());
+
+        PowerMockito.mockStatic(Calendar.class);
+        Mockito.when(Calendar.getInstance().getTime()).thenReturn(d);
+
+        PowerMockito.mockStatic(System.class);
+        Mockito.when(System.currentTimeMillis()).thenReturn(d.getTime());
+
+        // set available total garage
+        reservationService.handleMessage(newMessage_MSG_GET_PARKING_SPOTS());
+
+        Reservation aaa = new Reservation();
+        aaa.setCustomerName("somebody");
+        aaa.setVehicleID("5tgb");
+        aaa.setStartTime("2017:08:08:14:00");//"yyyy:MM:dd:HH:mm"
+        aaa.setEndTime("2017:08:08:16:00");
+        Mockito.when(reservationStore.getReservations()).thenReturn(list);
+        reservationService.handleMessage(newMessage_MSG_NEW_RSVP(aaa));
+        Assert.assertEquals(Boolean.FALSE, aaa.isComplete());
+        Mockito.verify(msgBus).generateMessage(newMessage_Error_ReservationService_verifyReservation(), null);
+    }
+
+    /**
+     * TS01-TC09 - Reservation try when end time > current time + week
+     */
+    @Test
+    public void test_Reservation_try_when_end_time_more_than_cur_plus_week() {
+        Vector list = new Vector<Reservation>();
+
+        LocalDateTime ldt = LocalDateTime.of(2017, Month.AUGUST, 1, 15, 0);
+        Date d = Date.from(ldt.atZone(ZoneId.systemDefault()).toInstant());
+
+        PowerMockito.mockStatic(Calendar.class);
+        Mockito.when(Calendar.getInstance().getTime()).thenReturn(d);
+
+        PowerMockito.mockStatic(System.class);
+        Mockito.when(System.currentTimeMillis()).thenReturn(d.getTime());
+
+        // set available total garage
+        reservationService.handleMessage(newMessage_MSG_GET_PARKING_SPOTS());
+
+        Reservation aaa = new Reservation();
+        aaa.setCustomerName("somebody");
+        aaa.setVehicleID("5tgb");
+        aaa.setStartTime("2017:08:04:15:00");//"yyyy:MM:dd:HH:mm"
+        aaa.setEndTime("2017:08:05:12:00");
+        Mockito.when(reservationStore.getReservations()).thenReturn(list);
+        reservationService.handleMessage(newMessage_MSG_NEW_RSVP(aaa));
+        Assert.assertEquals(Boolean.TRUE, aaa.isComplete());
+        Mockito.verify(msgBus,Mockito.never()).generateMessage(newMessage_Error_ReservationService_verifyReservation(), null);
     }
 }
